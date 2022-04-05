41b8c6f05f383df473ee006fb7614fc744e075e9
BaeBrowns <j.bruinsma.6@student.rug.nl>
2022-04-05 17:19:44 +0200

    Added comments and finalized the model trainer

--- dataloader.py
+++ dataloader.py
@@ -15,14 +15,15 @@ import pandas as pd
 class esnli(Dataset):
     def __init__(
         self,
-        frac_of_data=0.01,
+        frac_of_data=0.0004, # Set the fraction of data to be used for training (right now is very low, so it will only use 4 batches)
     ):
-        
+        # Load dataset and turn it to a pandas dataframe 
         dataset = load_dataset("esnli")
         self.train_df = pd.DataFrame.from_dict(dataset["train"])
         self.val_df = pd.DataFrame.from_dict(dataset["validation"])
         self.test_df = pd.DataFrame.from_dict(dataset["test"])
         
+        # Refromat all data objects
         self.reformat_data()
         
         if frac_of_data < 1.0:
@@ -31,7 +32,7 @@ class esnli(Dataset):
             self.val_df = self.val_df.sample(frac=frac_of_data, replace=False, random_state=42)
             self.test_df = self.test_df.sample(frac=frac_of_data, replace=False, random_state=42)
         
-        
+        # Initialize the tokenizer
         self.tokenizer = AutoTokenizer.from_pretrained("t5-base", use_fast=True)
         
         self.train_data = None
@@ -40,12 +41,25 @@ class esnli(Dataset):
         self.initialize_data()
 
     def initialize_data(self):
+        """ Initialize the data.
+        """
+        
         self.train_data = self.load_data(self.train_df)
         self.val_data = self.load_data(self.val_df)
         self.test_data = self.load_data(self.test_df)
         
         
     def get_data_loaders(self, batch_size=64, shuffle=True):
+        """ Initializes the data loaders, shuffles the samples if desired and puts everyhing in batches.
+
+        Args:
+            batch_size (int, optional): Batch size in the dataset. Defaults to 64.
+            shuffle (bool, optional): Indicates if the dataset need to be shuffled. Defaults to True.
+
+        Returns:
+            DataLoaders: Returns the data loaders.
+        """
+        
         print("initializing train data loader")
         train_loader = DataLoader(
             self.train_data, shuffle=shuffle, batch_size=batch_size
@@ -60,6 +74,14 @@ class esnli(Dataset):
         return train_loader, val_loader, test_loader
         
     def load_data(self, df):
+        """ Loads the data into a TensorDataset. It takes the dataframe, tokenizes the input and labels and puts them in a tensor.
+
+        Args:
+            df (Pandas dataframe): The dataframe to be loaded.
+
+        Returns:
+           TensorDataset: The converted tensordataset
+        """
         input_ids = []
         attention_mask = []
         token_type_ids = []
@@ -69,10 +91,20 @@ class esnli(Dataset):
         hypothesis_list = df["hypothesis"].tolist()
         explanation_list = df["explanation_1"].tolist()
         
-        for (premise, hypothesis, explanation) in zip(premise_list, hypothesis_list, explanation_list):
-            # Concatenate premise and hypothesis with separator token </s>
-            premise_hypothesis = f"{premise} </s> {hypothesis} </s>"
+        # Loop through all lists simultaniously
+        for (
+            premise, 
+            hypothesis, 
+            explanation
+            ) in zip(
+                premise_list, 
+                hypothesis_list, 
+                explanation_list
+                ):
+            # Concatenate premise and hypothesis with separator token :: and add an end of line token
+            premise_hypothesis = f"{premise} :: {hypothesis} </s>"
             
+            # Tokenize the premise and hypothesis
             hypothesis_premise_tokens = self.tokenizer.encode_plus(
                 premise_hypothesis,
                 truncation=True, 
@@ -80,6 +112,7 @@ class esnli(Dataset):
                 max_length=256,
                 )
             
+            # Tokenize the target explanation
             target_encoding = self.tokenizer.encode_plus(
                 explanation,
                 truncation=True,
@@ -88,17 +121,22 @@ class esnli(Dataset):
                 max_length=256,
             )
             
-        
+            # For the input reurn the input_ids, attention_mask and token_type_ids as tensor to the list
             token_type_ids.append(torch.Tensor(hypothesis_premise_tokens.token_type_ids))
             attention_mask.append(torch.Tensor(hypothesis_premise_tokens.attention_mask))
             input_ids.append(torch.Tensor(hypothesis_premise_tokens.input_ids))
+            
+            # For the target return the target_ids as tensor to the list
             target_ids.append(torch.Tensor(target_encoding.input_ids))
         
+        # Pad the sequences such that they are all of equal length. 
+        # Batch_first means that we use the batch dimension as the first dimension.
         token_type_ids = pad_sequence(token_type_ids, batch_first=True)
         attention_mask = pad_sequence(attention_mask, batch_first=True)
         input_ids = pad_sequence(input_ids, batch_first=True)
         target = pad_sequence(target_ids, batch_first=True)
         
+        # Convert the tensors to tensordataset
         dataset = TensorDataset(
             input_ids,
             attention_mask,
@@ -108,8 +146,60 @@ class esnli(Dataset):
         return dataset
 
     def reformat_data(self):
-       
-        # for evey column in dataframe, train_df, set the datatype to str except for the label column
-        self.test_df[["premise", "hypothesis", "explanation_1", "explanation_2", "explanation_3"]] = self.train_df[["premise", "hypothesis", "explanation_1", "explanation_2", "explanation_3"]].astype(str)
-        self.val_df[["premise", "hypothesis", "explanation_1", "explanation_2", "explanation_3"]] = self.val_df[["premise", "hypothesis", "explanation_1", "explanation_2", "explanation_3"]].astype(str)   
-        self.train_df[["premise", "hypothesis", "explanation_1"]] = self.train_df[["premise", "hypothesis", "explanation_1"]].astype(str)
+        """ This will set every object in the dataset to be a list of strings. Except the label, which will stay an Int. 
+        """
+
+        self.test_df[
+            [
+                "premise", 
+                "hypothesis", 
+                "explanation_1", 
+                "explanation_2", 
+                "explanation_3"
+            ]
+        ] = self.train_df[
+            [
+                "premise", 
+                "hypothesis", 
+                "explanation_1", 
+                "explanation_2", 
+                "explanation_3"
+                ]
+            ].astype(
+                str
+                )
+        
+        self.val_df[
+            [
+                "premise",
+                "hypothesis",
+                "explanation_1",
+                "explanation_2",
+                "explanation_3"
+                ]
+        ] = self.val_df[
+            [
+                "premise", 
+                "hypothesis",
+                "explanation_1", 
+                "explanation_2", 
+                "explanation_3"]
+            ].astype(
+                str
+                )   
+        
+        self.train_df[
+            [
+                "premise",
+                "hypothesis", 
+                "explanation_1"
+                ]
+        ] = self.train_df[
+            [
+                "premise", 
+                "hypothesis", 
+                "explanation_1"
+                ]
+            ].astype(
+                str
+                )
--- t5_trainer.py
+++ t5_trainer.py
@@ -13,27 +13,47 @@ import time
 
 
 def train_t5(model, train_loader, val_loader, optimizer, wandb):
-    
+    """Fine-tunes the T5 model. 
+
+    Args:
+        model : The model to be fine-tuned.
+        train_loader (DataLoader): The train data loader.
+        val_loader (DataLoader): The validation data loader.
+        optimizer (torch.optim): The desired optimizer.
+        wandb : The wandb logger.
+    """
+    # Start training (this is just like normal training with epochs only now its being tracked)
     for epoch in range(wandb.config.epochs):
         start = time.time()
         print("Epoch: ", epoch)
+        # Set the model to train
         model.train()
         total_train_loss = 0 
-        for batch, in (
+        print("training loader:", len(train_loader))
+        
+        count = 0 
+        # Iterate over the batches in the loader
+        for batch, (
             token_type_ids, 
             attention_mask,
             input_ids,
             target_ids,
         ) in enumerate(train_loader):
+            print("batch number:", count)
+            # Set the optimizer to zero grad to avoid accumulating gradients
             optimizer.zero_grad()
-            token_type_ids = token_type_ids.to(device).long
-            attention_mask = attention_mask.to(device).long
-            input_ids = input.ids.to(device).long
-            target_ids = target_ids.to(device).long
             
+            # Set all tensors to the device
+            token_type_ids = token_type_ids.to(device).long()
+            attention_mask = attention_mask.to(device).long()
+            input_ids = input_ids.to(device).long()
+            target_ids = target_ids.to(device).long()
+            
+            # Set the target ids and labels 
             y_ids = target_ids[:, :-1].contiguous()
-            lm_labels = target_ids[:, 1:].clone.detach()
+            lm_labels = target_ids[:, 1:].clone().detach()
             
+            # Forward pass
             outputs = model(
                 input_ids=input_ids,
                 attention_mask= attention_mask,
@@ -41,31 +61,45 @@ def train_t5(model, train_loader, val_loader, optimizer, wandb):
                 labels=lm_labels,
             )
             
+            # Get the loss
             loss = outputs[0]
+            
+            # Backward pass
             loss.backward()
+            
+            # Update the parameters
             optimizer.step()
             
+            # Add the loss to the total loss
             total_train_loss += loss.item()
-    
+            
+            count+=1
+ 
+        # Calculate the average loss
         train_loss = total_train_loss / len(train_loader)
         
+        # Evaluate the model on the validation set
         model.eval()
         total_val_loss = 0
+        
+        # Avoid using the gradient for validation. Otherwise we are still training the model
         with torch.no_grad():
-            for batch, in (
+            for batch, (
+                input_ids, 
                 token_type_ids, 
                 attention_mask,
-                input_ids,
                 target_ids,
             ) in enumerate(val_loader):
-                token_type_ids = token_type_ids.to(device).long
-                attention_mask = attention_mask.to(device).long
-                input_ids = input_ids.to(device).long
-                target_ids = target_ids.to(device).long
+                token_type_ids = token_type_ids.to(device).long()
+                attention_mask = attention_mask.to(device).long()
+                input_ids = input_ids.to(device).long()
+                target_ids = target_ids.to(device).long()
                 
+                # Set the target ids and labels 
                 y_ids = target_ids[:, :-1].contiguous()
-                lm_labels = target_ids[:, 1:].clone.detach()
+                lm_labels = target_ids[:, 1:].clone().detach()
                 
+                # Forward pass
                 outputs = model(
                     input_ids=input_ids,
                     attention_mask= attention_mask,
@@ -73,14 +107,19 @@ def train_t5(model, train_loader, val_loader, optimizer, wandb):
                     labels=lm_labels,
                 )
                 
+                # Get the loss
                 loss = outputs[0]
+                
+                # Add the loss to the total loss
                 total_val_loss += loss.item()
 
+        # Calculate the average loss
         val_loss = total_val_loss / len(val_loader)
         end = time.time()
         # return time in hours and minutes
         hours, rem = divmod(end-start, 3600)
 
+        # Log the training loss and validation loss
         wandb.log(
             {
                 "val_loss": val_loss,
@@ -91,11 +130,20 @@ def train_t5(model, train_loader, val_loader, optimizer, wandb):
                 "val_size": len(val_loader),
             }
         )
-        
+           
+            
 def evaluate(model, test_loader, wandb):
+    """Evaluating the model on the test set.
+
+    Args:
+        model: The model to be used for evaluation.
+        test_loader (DataLoader): The test data loader.
+        wandb: The wandb logger.
+    """
     model.eval()
     total_test_loss = 0 
-    
+
+    # Avoid using the gradient for evaluation. Otherwise we are still training the model
     with torch.no_grad():
         for batch, (
             token_type_ids, 
@@ -103,14 +151,17 @@ def evaluate(model, test_loader, wandb):
             input_ids,
             target_ids,
         ) in enumerate(test_loader):
-            token_type_ids = token_type_ids.to(device).long
-            attention_mask = attention_mask.to(device).long
-            input_ids = input_ids.to(device).long
-            target_ids = target_ids.to(device).long
+            # Set all tensors to the device
+            token_type_ids = token_type_ids.to(device).long()
+            attention_mask = attention_mask.to(device).long()
+            input_ids = input_ids.to(device).long()
+            target_ids = target_ids.to(device).long()
             
+            # Set the target ids and labels 
             y_ids = target_ids[:, :-1].contiguous()
-            lm_labels = target_ids[:, 1:].clone.detach()
+            lm_labels = target_ids[:, 1:].clone().detach()
             
+            # Forward pass
             outputs = model(
                 input_ids=input_ids,
                 attention_mask= attention_mask,
@@ -118,10 +169,16 @@ def evaluate(model, test_loader, wandb):
                 labels=lm_labels,
             )
             
+            # Get the loss
             loss = outputs[0]
+            
+            # Add the loss to the total loss
             total_test_loss += loss.item()
         
+        # Calculate the average loss
         test_loss = total_test_loss / len(test_loader)
+        
+        # Log the test loss
         wandb.log(
             {
                 "test_loss": test_loss,
@@ -130,12 +187,12 @@ def evaluate(model, test_loader, wandb):
             )
     
             
-                
-        
-
 
 if __name__ == "__main__":
     
+    # The weights and biases (wandb) libary let's us keep track of the model's hyperparameters and configurations during training. 
+    # You might want to make your own account here: https://wandb.ai/
+    # After that you can set project and entity according to your own settings.
     wandb.init(project="NLP-final-project", entity="baebrowns")
     wandb.config.epochs = 5
     wandb.config.batch_size = 64
@@ -148,23 +205,22 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Device: ", device)
 
+    # Load the dataset from dataloader
     dataset = esnli()
     
+    # Load dataloaders
     train_loader, val_loader, test_loader = dataset.get_data_loaders(batch_size = wandb.config.batch_size)
     
     # set model configuration and model
-    config = AutoConfig.from_pretrained("t5-base") 
+    config = AutoConfig.from_pretrained("t5-base", ) 
     model = T5ForConditionalGeneration.from_pretrained("t5-base", config=config)
     wandb.watch(model)
     
-
-    
+    # Use AdamW optimizer
     optimizer = AdamW(model.parameters(), lr=wandb.config.lr)
     
     model.to(device)
-    
-    
-    
+        
     train_t5(model, train_loader, val_loader, optimizer, wandb)
     
     evaluate(model, test_loader, wandb)
